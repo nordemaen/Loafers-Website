61966fbc282062ff039b701c4112519f93770752
Cameron Shearer <SHE16003894@btc.ac.uk>
2022-04-04 16:32:09 +0100

    Push Exisiting Content

--- public/assets/js/newsletter.js
+++ public/assets/js/newsletter.js
@@ -4,7 +4,7 @@ buttonSubscribe.addEventListener("click", function() {
     let checkInput = document.getElementById("subscribe-input").value;
 
     if (checkInput === "") {
-        let changeStyle = document.getElementById("subscribe-input").style.color = "red";
+        let changeStyle = document.getElementById("subscribe-input").style.color = "gray";
     } else {
         alert(`Thank you, ${checkInput}. You've been added to our mailing list!`);
     }
